2974cda18fdc78edea17f428431010b41c9140c1
Yanir Shmulevich <yanir911@gmail.com>
2026-06-22 20:12:49 +0300

    add swagger api docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
@@ -7,10 +8,21 @@ async function bootstrap() {
 
   app.enableCors();
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('LeapAI API')
+    .setDescription('Backend API documentation for LeapAI')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api/docs', app, swaggerDocument);
+
   const port = process.env.PORT || 3001;
   await app.listen(port);
 
   console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Swagger docs are running on http://localhost:${port}/api/docs`);
 }
 
 bootstrap();
